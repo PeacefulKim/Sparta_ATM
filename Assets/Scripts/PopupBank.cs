b4a7a14dd9535f3ec515893ca85b53b7c73ac261
PeacefulKim <corbata@naver.com>
2025-11-26 09:51:37 +0900

    feat: 송금 기능

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -32,6 +34,11 @@ public class PopupBank : MonoBehaviour
         atmPopup.SetActive(false);
         withdrawPopup.SetActive(true);
     }
+    public void OnClickSend()
+    {
+        atmPopup.SetActive(false);
+        sendPopup.SetActive(true);
+    }
     public void OnClickBack()
     {
         depositPopup.SetActive(false);
@@ -62,8 +69,40 @@ public class PopupBank : MonoBehaviour
         userData.balance -= money;
         userData.cash += money;
     }
-    public void Send(int money)
+    public void Send()
     {
+        if (receiverInput.text == null || sendInput.text == null)
+        {
+            errorTxt.text = "입력 정보를 확인해주세요.";
+            return;
+        }
+
+        UserData userData = GameManager.Instance.userData;
+        int.TryParse(sendInput.text, out int _money);
+        if(_money > userData.balance)
+        {
+            errorTxt.text = "잔액이 부족합니다.";
+            return;
+        }
+        try
+        {
+            string path = Application.dataPath + "/Data/" + receiverInput.text + ".json";
+            string database = File.ReadAllText(path);
+            Debug.Log(database);
+            UserData receiver = JsonConvert.DeserializeObject<UserData>(database);
+
+            receiver.balance += _money;
+            userData.balance -= _money;
+
+            string json = JsonConvert.SerializeObject(receiver);
+            File.WriteAllText(path, json);
+        }
+        catch (FileNotFoundException)
+        {
+            errorTxt.text = "유효하지 않은 대상입니다.";
+            return;
+        }
+
 
     }
 
